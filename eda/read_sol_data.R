bcf04e8f39d71a56a218dd5ddb839840a821d8f5
llopezmanzanares <luislmf@gmail.com>
2021-12-29 15:22:26 +0000

    File transformed. Data output is tidy (date, time and ort / ocs [sun rise, sun set]

--- eda/read_sol_data.R
+++ eda/read_sol_data.R
@@ -1,4 +1,5 @@
 # read file of sunrise and sunset hours
+# output: date, time and sunrise/sunset (ort/ocs)
 
 library(tidyverse)
 
@@ -25,9 +26,30 @@ names(ss_ds) <-
       )
     )
 
-ss_ds <- ss_ds %>% 
+s_rs <- ss_ds %>% 
   pivot_longer(
     -n_dia,
     names_to = "tipo",
     values_to = "hora"
-  )
+  ) %>% 
+  filter(hora != " ")
+
+s_rs <- s_rs %>%  
+  mutate(
+    month = str_sub(tipo, 1, 3),
+    month = match(month, month.abb),
+    rs = str_sub(tipo, 5, 7),
+    hora = as.numeric(hora),
+    hour = hora %/% 100,
+    minute = hora %% 100,
+    time = lubridate::hm(str_c(
+      hour, minute,
+      sep = ":")
+      ),
+    fecha = str_c(
+      "2007", month, n_dia,
+      sep = "/"
+    ),
+    fecha = as.Date(fecha, "%Y/%m/%e")
+  ) %>% 
+  select(fecha, time, rs)
